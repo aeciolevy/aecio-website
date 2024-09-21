6b55ea91d38ee52acba9d5f2f4c91de6e4327381
Aecio Levy <aeciolevy@gmail.com>
2024-09-21 19:12:19 -0400

    Added language-specific components for About page Updated AboutPage component to conditionally render language-specific components (AboutPT and AboutEN) based on the selected language (PT or EN)

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,6 +1,8 @@
 import { Language } from "@/app/types/language";
 import { Header } from "@/app/[lang]/components/layout/Header";
 import { getDictionary } from "@/app/[lang]/dictionaries";
+import { AboutPT } from "./components/AboutPT";
+import { AboutEN } from "./components/AboutEN";
 
 type Props = {
   params: {
@@ -14,8 +16,9 @@ export default async function AboutPage({ params: { lang } }: Props) {
   return (
     <>
       <Header lang={lang} dictionary={dictionary} />
-      <main>
-        <h1> About page </h1>
+      <main className="p-16 lg:p-24 xl:p-32" >
+        {lang === Language.PT && <AboutPT />}
+        {lang === Language.EN && <AboutEN />}
       </main>
 
     </>
