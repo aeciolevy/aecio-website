8af84c96f2ab346b4b5156032ca5735e3c39f8a7
Aecio Levy <aeciolevy@gmail.com>
2024-10-15 07:34:09 -0400

    • Deleted BlogCard.tsx file from app/[lang]/blog/components/.  • Updated import path in EnglishBlog.tsx to use the new location of    BlogCard.  • Added eslint-config-prettier version 9.1.0 to package.json and    package-lock.json.

--- app/[lang]/blog/components/EnglishBlog.tsx
+++ app/[lang]/blog/components/EnglishBlog.tsx
@@ -1,22 +1,26 @@
 import { Post, allPosts } from "content-collections";
 import Link from "next/link";
-import { BlogCard } from "./BlogCard";
+import { BlogCard } from "@/app/[lang]/components/blog/BlogCard";
 import { sortDescending } from "@/app/lib/parseDate";
 
 export const EnglishBlog = () => {
-  const publishedAndSortedPosts = allPosts.filter((post: Post) => post.published).sort(sortDescending);
+  const publishedAndSortedPosts = allPosts
+    .filter((post: Post) => post.published)
+    .sort(sortDescending);
 
-  console.log('### allPosts', allPosts)
   return (
     <div className="w-full h-full p-12">
       <div className="grid grid-cols-2 md:grid-cols-3">
         {publishedAndSortedPosts.map((post: Post) => (
           <Link href={`/blog/${post._meta.path}`} key={post._meta.path}>
-            <BlogCard title={post.title} summary={post.summary} thumbnail={post.thumbnail} />
+            <BlogCard
+              title={post.title}
+              summary={post.summary}
+              thumbnail={post.thumbnail}
+            />
           </Link>
         ))}
       </div>
     </div>
   );
-}
-
+};
