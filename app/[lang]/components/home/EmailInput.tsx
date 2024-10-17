27f21acb903c67f9aaa6d0208398fe808ef35548
Aecio Levy <aeciolevy@gmail.com>
2024-10-17 19:51:52 -0400

    Email input file

---
+++ app/[lang]/components/home/EmailInput.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const EmailInput = (props: React.HTMLProps<HTMLInputElement>) => {
+  const { placeholder } = props;
+  return (
+    <>
+      <input
+        type="email"
+        className="bg-gray-800 w-full sm:w-3/4 rounded-md text-gray-400 text-base font-normal p-4 focus:outline-none"
+        placeholder={`${placeholder || "Enter your email"} `}
+        {...props}
+      />
+    </>
+  );
+};
