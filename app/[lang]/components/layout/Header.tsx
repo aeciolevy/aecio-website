ca40ce190de3292a13102d07249217d922d19fbb
Aecio Levy <aeciolevy@gmail.com>
2024-11-01 06:49:49 -0400

    Update Header Component for Language-Specific Blog Link

--- app/[lang]/components/layout/Header.tsx
+++ app/[lang]/components/layout/Header.tsx
@@ -12,6 +12,8 @@ type HeaderProps = {
 
 export const Header = ({ dictionary, lang }: HeaderProps) => {
   const linkStyle = "text-[#dfe5f5] hover:text-primary-500";
+  const blogHref = lang === Language.EN ? "/blog" : "/cartas";
+
   return (
     <div className="flex justify-between items-center py-4 px-4 sm:px-16 w-full bg-slate-950">
       <Link href={`/${lang}`} className="cursor-pointer" >
@@ -19,7 +21,8 @@ export const Header = ({ dictionary, lang }: HeaderProps) => {
       </Link>
       <HamburgerNavMenu />
       <div className="hidden sm:flex items-start gap-16">
-        <Link href="/blog" className={`${linkStyle}`}>{dictionary.letters}</Link>
+
+        <Link href={blogHref} className={`${linkStyle}`}>{dictionary.letters}</Link>
         {lang === Language.PT && (
           <Link href="/composicoes" className={`${linkStyle}`}>{dictionary.songLyrics}</Link>
         )
