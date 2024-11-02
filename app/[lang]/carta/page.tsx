de8491ace3c9b5d89b8c58f71c2501a5743cc51a
Aecio Levy <aeciolevy@gmail.com>
2024-11-02 18:33:41 -0400

    Carta files

---
+++ app/[lang]/carta/page.tsx
@@ -0,0 +1,29 @@
+import { Header } from "../components/layout/Header";
+import { getDictionary } from "../dictionaries";
+import { Language } from "@/app/types/language";
+import { PortugueseBlog } from "../blog/components/PortugueseBlog";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Acervo de Cartas | Aécio Levy",
+  description: "Explore o acervo de cartas de Aécio Levy. Sobre trabalho remoto, performance e evolução humana",
+  keywords: "cartas, trabalho remoto, performance, evolução humana",
+}
+
+type CartasPageProps = {
+  params: {
+    lang: Language;
+  };
+};
+
+export default async function CartasPage({ params: { lang } }: CartasPageProps) {
+  const dictionary = await getDictionary(lang);
+  return (
+    <>
+      <Header lang={lang} dictionary={dictionary} />
+      {lang === Language.PT && <PortugueseBlog />}
+    </>
+
+  )
+}
+
